2f3a3db66a59e2fb570bbdd572d76a99aaf45f4e
Luca <luca.tagliavini5@studio.unibo.it>
2023-05-16 12:14:48 +0200

    actually fetch images descriptions from last.fm

--- web/src/tasks/lastfm_artist_image.rs
+++ web/src/tasks/lastfm_artist_image.rs
@@ -20,6 +20,8 @@ use tag::{map::tags_from_artist, tag_to_string_map};
 lazy_static! {
     static ref IMAGE_SELECTOR: Selector =
         Selector::parse("ul.image-list li.image-list-item-wrapper a.image-list-item").unwrap();
+    static ref IMAGE_DESCRIPTION_SELECTOR: Selector =
+        Selector::parse(".gallery-image-description").unwrap();
 }
 static LASTFM_IMAGE_ATTEMPT_SIZE: usize = 4096;
 
@@ -36,7 +38,28 @@ pub async fn all_data(db: &DbConn) -> Result<Vec<Data>> {
         .collect())
 }
 
-fn img_url(url: &url::Url) -> Result<(url::Url, String)> {
+async fn get_html(url: url::Url) -> Result<String> {
+    let res = send_request(Request::new(Method::GET, url)).await?;
+    if !res.status().is_success() {
+        bail!(
+            "Last.fm request returned non-success error code: {} {}",
+            res.status(),
+            res.text().await?
+        );
+    }
+    res.text()
+        .await
+        .wrap_err(eyre!("Could not read response as text"))
+}
+
+struct UrlData {
+    image_page: url::Url,
+    image_url: url::Url,
+    image_id: String,
+}
+
+fn img_url(url: &url::Url) -> Result<UrlData> {
+    let src_url = url.clone();
     if let Some(path) = url.path_segments() {
         if let Some(id) = path.last() {
             let url = format!(
@@ -44,21 +67,48 @@ fn img_url(url: &url::Url) -> Result<(url::Url, String)> {
                 LASTFM_IMAGE_ATTEMPT_SIZE, id, id
             )
             .parse()?;
-            return Ok((url, id.to_string()));
+            return Ok(UrlData {
+                image_page: src_url,
+                image_url: url,
+                image_id: id.to_string(),
+            });
         }
     }
 
     Err(eyre!(
         "Could not extract image id from last.fm url: {}",
-        url
+        src_url
     ))
+}
+
+fn extract_description(html: &String) -> Option<String> {
+    let html = Html::parse_document(html.as_str());
+    html.select(&IMAGE_DESCRIPTION_SELECTOR)
+        .next()
+        .map(|p| p.text().collect())
+}
+
+fn get_urls(url: &mut url::Url, html: &String, artist_id: Uuid) -> Result<Vec<UrlData>> {
+    let html = Html::parse_document(html.as_str());
+    let mut urls = Vec::new();
+    for anchor in html.select(&IMAGE_SELECTOR) {
+        if let Some(abs_path) = anchor.value().attr("href") {
+            url.set_path(abs_path);
+            tracing::trace! {image_url = %url, artist = %artist_id, "Found artist image"};
+            urls.push(img_url(&url)?);
+        }
+    }
+    Ok(urls)
 }
 
 async fn download(
     db: &DbConn,
     artist_id: Uuid,
-    url: &url::Url,
-    id: String,
+    UrlData {
+        image_page,
+        image_url,
+        image_id,
+    }: UrlData,
 ) -> Result<Option<entity::Image>> {
     let library = &get_settings()?.library;
     let artist = entity::ArtistEntity::find_by_id(artist_id)
@@ -76,10 +126,13 @@ async fn download(
         )?)
         .join(".images");
     mkdirp(&artist_images_path)?;
-    let image_path = artist_images_path.join(id);
+    let image_path = artist_images_path.join(image_id);
 
     if !image_path.exists() {
-        let bytes = get(url.to_owned()).await?.bytes().await?;
+        let text = get_html(image_page).await?;
+        let description = extract_description(&text);
+
+        let bytes = get(image_url.to_owned()).await?.bytes().await?;
         let format = infer::get(&bytes)
             .and_then(|t| t.mime_type().parse().ok())
             .and_then(ImageFormat::from_mime)
@@ -94,12 +147,12 @@ async fn download(
             .decode()?;
         let path_str = path_to_str(&image_path)?;
         img.save_with_format(image_path, format.into())?;
+
         return Ok(Some(entity::Image {
             id: sha256::digest(path_str.as_str()),
             role: PictureType::Artist.to_string(),
             format,
-            // TODO: actually fetch description from last.fm
-            description: None,
+            description,
             width: img.width(),
             height: img.height(),
             size: len as u32,
@@ -112,42 +165,18 @@ async fn download(
     Ok(None)
 }
 
-fn get_urls(url: &mut url::Url, text: &String, artist_id: Uuid) -> Result<Vec<(url::Url, String)>> {
-    let html = Html::parse_document(text.as_str());
-    let mut urls = Vec::new();
-    for anchor in html.select(&IMAGE_SELECTOR) {
-        if let Some(abs_path) = anchor.value().attr("href") {
-            url.set_path(abs_path);
-            tracing::trace! {image_url = %url, artist = %artist_id, "Found artist image"};
-            urls.push(img_url(&url)?);
-        }
-    }
-    Ok(urls)
-}
-
 pub async fn run(db: &DbConn, Data(artist_id, url): Data) -> Result<()> {
     tracing::trace!(%artist_id, %url, "Fetching artist images from lastfm");
     let mut url = (url.clone() + "/").parse::<url::Url>()?.join("+images")?;
-    let res = send_request(Request::new(Method::GET, url.to_owned())).await?;
-    if !res.status().is_success() {
-        bail!(
-            "Last.fm request returned non-success error code: {} {}",
-            res.status(),
-            res.text().await?
-        );
-    }
-    let text = res
-        .text()
-        .await
-        .wrap_err(eyre!("Could not read response as text"))?;
+    let text = get_html(url.to_owned()).await?;
 
     // TODO: paginate over lastfm, setting limit
     let urls = get_urls(&mut url, &text, artist_id)?;
-    for (image_url, image_id) in urls.into_iter() {
-        let new_image = download(db, artist_id, &image_url, image_id.clone()).await?;
+    for url_data in urls.into_iter() {
+        let new_image = download(db, artist_id, url_data).await?;
         if let Some(image) = new_image {
             let id = image.id.to_owned();
-            tracing::trace! {path = ?image.path, %image_id, artist_id = %artist_id, "Downloaded image for artist"};
+            tracing::trace! {path = ?image.path, artist_id = %artist_id, "Downloaded image for artist"};
             entity::ImageEntity::insert(image.into_active_model())
                 .on_conflict(entity::conflict::IMAGE_CONFLICT_1.to_owned())
                 .on_conflict(entity::conflict::IMAGE_CONFLICT_2.to_owned())
